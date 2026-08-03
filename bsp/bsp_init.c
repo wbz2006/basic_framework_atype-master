54ddb15c7d4aaa9479a510ebd2751f6a8647ca1c
wbz2006 <wbz2006wbz@163.com>
2026-08-03 15:12:01 +0800

    fix：去除BSPInit中不必要部分

--- bsp/bsp_init.c
+++ bsp/bsp_init.c
@@ -2,11 +2,9 @@
 #include "bsp_log.h"
 #include "bsp_dwt.h"
 #include "tim.h"
-#include "oled.h"
 // CAN和串口会在注册实例的时候自动初始化,不注册不初始化
 void BSPInit()
 {
     DWT_Init(168);
     BSPLogInit();
-    OLED_init();
 }
