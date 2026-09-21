5f2e433bb1591865ff4f3aed55a782359260cd04
wbz2006 <wbz2006wbz@163.com>
2026-09-21 22:43:08 +0800

    feat：更新达妙电机库：新增MIT控制模式

--- modules/motor/DMmotor/dmmotor.c
+++ modules/motor/DMmotor/dmmotor.c
@@ -1,27 +1,42 @@
 #include "dmmotor.h"
-#include "memory.h"
-#include "general_def.h"
-#include "user_lib.h"
-#include "string.h"
-#include "stdlib.h"
+
 #include "bsp_log.h"
-#include "bsp_dwt.h"
+#include "daemon.h"
+#include "general_def.h"
+#include "memory.h"
+#include "stdlib.h"
+#include "string.h"
+#include "user_lib.h"
 
 static uint8_t idx;
 static DMMotorInstance *dm_motor_instance[DM_MOTOR_CNT];
 
-static uint16_t float_to_uint(float x, float x_min, float x_max, uint8_t bits)
+static uint16_t float_to_uint(
+    float x,
+    float x_min,
+    float x_max,
+    uint8_t bits)
 {
-    float span = x_max - x_min;
-    float offset = x_min;
-    return (uint16_t)((x - offset) * ((float)((1 << bits) - 1)) / span);
+    const float span = x_max - x_min;
+    const float offset = x_min;
+
+    return (uint16_t)((x - offset) *
+                      ((float)((1U << bits) - 1U)) /
+                      span);
 }
 
-static float uint_to_float(int x_int, float x_min, float x_max, int bits)
+static float uint_to_float(
+    int x_int,
+    float x_min,
+    float x_max,
+    int bits)
 {
-    float span = x_max - x_min;
-    float offset = x_min;
-    return ((float)x_int) * span / ((float)((1 << bits) - 1)) + offset;
+    const float span = x_max - x_min;
+    const float offset = x_min;
+
+    return ((float)x_int) * span /
+               ((float)((1U << bits) - 1U)) +
+           offset;
 }
 
 static void DMMotorSetMode(DMMotor_Mode_e cmd, DMMotorInstance *motor)
@@ -36,13 +51,16 @@ static void DMMotorDecode(CANInstance *motor_can)
     uint16_t tmp;
     uint8_t *rxbuff = motor_can->rx_buff;
     DMMotorInstance *motor = (DMMotorInstance *)motor_can->id;
-    DM_Motor_Measure_s *measure = &(motor->measure);
+    DM_Motor_Measure_s *measure = &motor->measure;
+
+    DaemonReload(motor->motor_daemon);
 
     measure->last_position = measure->position;
+
     tmp = (uint16_t)((rxbuff[1] << 8) | rxbuff[2]);
     measure->position = uint_to_float(tmp, DM_P_MIN, DM_P_MAX, 16);
 
-    tmp = (uint16_t)((rxbuff[3] << 4) | rxbuff[4] >> 4);
+    tmp = (uint16_t)((rxbuff[3] << 4) | (rxbuff[4] >> 4));
     measure->velocity = uint_to_float(tmp, DM_V_MIN, DM_V_MAX, 12);
 
     tmp = (uint16_t)(((rxbuff[4] & 0x0f) << 8) | rxbuff[5]);
@@ -50,6 +68,153 @@ static void DMMotorDecode(CANInstance *motor_can)
 
     measure->T_Mos = (float)rxbuff[6];
     measure->T_Rotor = (float)rxbuff[7];
+}
+
+static void DMMotorLostCallback(void *motor_ptr)
+{
+    DMMotorInstance *motor = (DMMotorInstance *)motor_ptr;
+
+    /*
+     * Keep the callback conservative. The periodic control function will
+     * transmit a zero command after the stop flag is set.
+     */
+    motor->pid_ref = 0.0f;
+    motor->stop_flag = MOTOR_STOP;
+}
+
+static float DMMotorGetAngleMeasure(
+    DMMotorInstance *motor,
+    Motor_Control_Setting_s *setting)
+{
+    if (setting->angle_feedback_source == OTHER_FEED &&
+        motor->other_angle_feedback_ptr != NULL)
+    {
+        return *motor->other_angle_feedback_ptr;
+    }
+
+    return motor->measure.position;
+}
+
+static float DMMotorGetSpeedMeasure(
+    DMMotorInstance *motor,
+    Motor_Control_Setting_s *setting)
+{
+    if (setting->speed_feedback_source == OTHER_FEED &&
+        motor->other_speed_feedback_ptr != NULL)
+    {
+        return *motor->other_speed_feedback_ptr;
+    }
+
+    return motor->measure.velocity;
+}
+
+static float DMMotorCalculateExternalOutput(DMMotorInstance *motor)
+{
+    float pid_measure;
+    float pid_ref = motor->pid_ref;
+    Motor_Control_Setting_s *setting = &motor->motor_settings;
+
+    if (setting->motor_reverse_flag == MOTOR_DIRECTION_REVERSE)
+    {
+        pid_ref *= -1.0f;
+    }
+
+    if ((setting->close_loop_type & ANGLE_LOOP) &&
+        setting->outer_loop_type == ANGLE_LOOP)
+    {
+        pid_measure = DMMotorGetAngleMeasure(motor, setting);
+        pid_ref = PIDCalculate(&motor->angle_PID, pid_measure, pid_ref);
+    }
+
+    if ((setting->close_loop_type & SPEED_LOOP) &&
+        (setting->outer_loop_type & (ANGLE_LOOP | SPEED_LOOP)))
+    {
+        if ((setting->feedforward_flag & SPEED_FEEDFORWARD) &&
+            motor->speed_feedforward_ptr != NULL)
+        {
+            pid_ref += *motor->speed_feedforward_ptr;
+        }
+
+        pid_measure = DMMotorGetSpeedMeasure(motor, setting);
+        pid_ref = PIDCalculate(&motor->speed_PID, pid_measure, pid_ref);
+    }
+
+    if ((setting->feedforward_flag & CURRENT_FEEDFORWARD) &&
+        motor->current_feedforward_ptr != NULL)
+    {
+        pid_ref += *motor->current_feedforward_ptr;
+    }
+
+    if (setting->close_loop_type & CURRENT_LOOP)
+    {
+        pid_ref = PIDCalculate(
+            &motor->current_PID,
+            motor->measure.torque,
+            pid_ref);
+    }
+
+    if (setting->feedback_reverse_flag == FEEDBACK_DIRECTION_REVERSE)
+    {
+        pid_ref *= -1.0f;
+    }
+
+    return pid_ref;
+}
+
+static void DMMotorSendMIT(
+    DMMotorInstance *motor,
+    float position_des,
+    float velocity_des,
+    float kp,
+    float kd,
+    float torque_des)
+{
+    if (motor->stop_flag == MOTOR_STOP)
+    {
+        position_des = 0.0f;
+        velocity_des = 0.0f;
+        kp = 0.0f;
+        kd = 0.0f;
+        torque_des = 0.0f;
+    }
+
+    LIMIT_MIN_MAX(position_des, DM_P_MIN, DM_P_MAX);
+    LIMIT_MIN_MAX(velocity_des, DM_V_MIN, DM_V_MAX);
+    LIMIT_MIN_MAX(kp, DM_KP_MIN, DM_KP_MAX);
+    LIMIT_MIN_MAX(kd, DM_KD_MIN, DM_KD_MAX);
+    LIMIT_MIN_MAX(torque_des, DM_T_MIN, DM_T_MAX);
+
+    motor->motor_send_mailbox.position_des =
+        float_to_uint(position_des, DM_P_MIN, DM_P_MAX, 16);
+    motor->motor_send_mailbox.velocity_des =
+        float_to_uint(velocity_des, DM_V_MIN, DM_V_MAX, 12);
+    motor->motor_send_mailbox.torque_des =
+        float_to_uint(torque_des, DM_T_MIN, DM_T_MAX, 12);
+    motor->motor_send_mailbox.Kp =
+        float_to_uint(kp, DM_KP_MIN, DM_KP_MAX, 12);
+    motor->motor_send_mailbox.Kd =
+        float_to_uint(kd, DM_KD_MIN, DM_KD_MAX, 12);
+
+    motor->motor_can_instace->tx_buff[0] =
+        (uint8_t)(motor->motor_send_mailbox.position_des >> 8);
+    motor->motor_can_instace->tx_buff[1] =
+        (uint8_t)motor->motor_send_mailbox.position_des;
+    motor->motor_can_instace->tx_buff[2] =
+        (uint8_t)(motor->motor_send_mailbox.velocity_des >> 4);
+    motor->motor_can_instace->tx_buff[3] =
+        (uint8_t)(((motor->motor_send_mailbox.velocity_des & 0x0f) << 4) |
+                  (motor->motor_send_mailbox.Kp >> 8));
+    motor->motor_can_instace->tx_buff[4] =
+        (uint8_t)motor->motor_send_mailbox.Kp;
+    motor->motor_can_instace->tx_buff[5] =
+        (uint8_t)(motor->motor_send_mailbox.Kd >> 4);
+    motor->motor_can_instace->tx_buff[6] =
+        (uint8_t)(((motor->motor_send_mailbox.Kd & 0x0f) << 4) |
+                  (motor->motor_send_mailbox.torque_des >> 8));
+    motor->motor_can_instace->tx_buff[7] =
+        (uint8_t)motor->motor_send_mailbox.torque_des;
+
+    CANTransmit(motor->motor_can_instace, 1);
 }
 
 void DMMotorCaliEncoder(DMMotorInstance *motor)
@@ -60,25 +225,50 @@ void DMMotorCaliEncoder(DMMotorInstance *motor)
 
 DMMotorInstance *DMMotorInit(Motor_Init_Config_s *config)
 {
-    DMMotorInstance *motor = (DMMotorInstance *)malloc(sizeof(DMMotorInstance));
+    DMMotorInstance *motor =
+        (DMMotorInstance *)malloc(sizeof(DMMotorInstance));
+
+    if (motor == NULL || config == NULL || idx >= DM_MOTOR_CNT)
+    {
+        return NULL;
+    }
+
     memset(motor, 0, sizeof(DMMotorInstance));
-    
+
     motor->motor_settings = config->controller_setting_init_config;
-    PIDInit(&motor->current_PID, &config->controller_param_init_config.current_PID);
-    PIDInit(&motor->speed_PID, &config->controller_param_init_config.speed_PID);
-    PIDInit(&motor->angle_PID, &config->controller_param_init_config.angle_PID);
-    motor->other_angle_feedback_ptr = config->controller_param_init_config.other_angle_feedback_ptr;
-    motor->other_speed_feedback_ptr = config->controller_param_init_config.other_speed_feedback_ptr;
+    PIDInit(&motor->current_PID,
+            &config->controller_param_init_config.current_PID);
+    PIDInit(&motor->speed_PID,
+            &config->controller_param_init_config.speed_PID);
+    PIDInit(&motor->angle_PID,
+            &config->controller_param_init_config.angle_PID);
+
+    motor->other_angle_feedback_ptr =
+        config->controller_param_init_config.other_angle_feedback_ptr;
+    motor->other_speed_feedback_ptr =
+        config->controller_param_init_config.other_speed_feedback_ptr;
+    motor->speed_feedforward_ptr =
+        config->controller_param_init_config.speed_feedforward_ptr;
+    motor->current_feedforward_ptr =
+        config->controller_param_init_config.current_feedforward_ptr;
 
     config->can_init_config.can_module_callback = DMMotorDecode;
     config->can_init_config.id = motor;
     motor->motor_can_instace = CANRegister(&config->can_init_config);
+
+    Daemon_Init_Config_s daemon_config = {
+        .callback = DMMotorLostCallback,
+        .owner_id = motor,
+        .reload_count = 10,
+    };
+    motor->motor_daemon = DaemonRegister(&daemon_config);
 
     DMMotorEnable(motor);
     DMMotorSetMode(DM_CMD_MOTOR_MODE, motor);
     DWT_Delay(0.1);
     DMMotorCaliEncoder(motor);
     DWT_Delay(0.1);
+
     dm_motor_instance[idx++] = motor;
     return motor;
 }
@@ -98,49 +288,96 @@ void DMMotorStop(DMMotorInstance *motor)
     motor->stop_flag = MOTOR_STOP;
 }
 
-void DMMotorOuterLoop(DMMotorInstance *motor, Closeloop_Type_e type)
+void DMMotorOuterLoop(DMMotorInstance *motor, Closeloop_Type_e close_loop_type)
 {
-    motor->motor_settings.outer_loop_type = type;
+    motor->motor_settings.outer_loop_type = close_loop_type;
 }
 
-void DMMotorControl()
+void DMMotorChangeFeed(
+    DMMotorInstance *motor,
+    Closeloop_Type_e loop,
+    Feedback_Source_e type)
 {
-    float pid_ref, set;
-    DMMotorInstance *motor;
-    Motor_Control_Setting_s *setting;
-    DMMotor_Send_s motor_send_mailbox;
-
-    for (size_t i = 0; i < idx; i++)
+    if (loop == ANGLE_LOOP)
     {
-        motor = dm_motor_instance[i];
-        setting = &motor->motor_settings;
+        motor->motor_settings.angle_feedback_source = type;
+    }
+    else if (loop == SPEED_LOOP)
+    {
+        motor->motor_settings.speed_feedback_source = type;
+    }
+    else
+    {
+        LOGERROR("[dm_motor] loop type error, check memory access and func param");
+    }
+}
 
-        pid_ref = motor->pid_ref;
-        set = pid_ref;
+void DMMotorSetMITConfig(
+    DMMotorInstance *motor,
+    const DMMotor_MIT_Config_s *config)
+{
+    if (motor == NULL || config == NULL)
+    {
+        return;
+    }
 
-        if (setting->motor_reverse_flag == MOTOR_DIRECTION_REVERSE)
-            set *= -1;
+    motor->mit_config = *config;
+}
 
-        LIMIT_MIN_MAX(set, DM_T_MIN, DM_T_MAX);
+void DMMotorSetMITRef(
+    DMMotorInstance *motor,
+    float position_des,
+    float velocity_des,
+    float kp,
+    float kd,
+    float torque_ff)
+{
+    const DMMotor_MIT_Config_s config = {
+        .position_des = position_des,
+        .velocity_des = velocity_des,
+        .kp = kp,
+        .kd = kd,
+        .torque_des = torque_ff,
+    };
 
-        motor_send_mailbox.position_des = float_to_uint(0, DM_P_MIN, DM_P_MAX, 16);
-        motor_send_mailbox.velocity_des = float_to_uint(0, DM_V_MIN, DM_V_MAX, 12);
-        motor_send_mailbox.torque_des = float_to_uint(set, DM_T_MIN, DM_T_MAX, 12);
-        motor_send_mailbox.Kp = 0;
-        motor_send_mailbox.Kd = 0;
+    DMMotorSetMITConfig(motor, &config);
+}
 
-        if (motor->stop_flag == MOTOR_STOP)
-            motor_send_mailbox.torque_des = float_to_uint(0, DM_T_MIN, DM_T_MAX, 12);
+void DMMotorControl(void)
+{
+    for (size_t i = 0; i < idx; ++i)
+    {
+        DMMotorInstance *motor = dm_motor_instance[i];
+        const float pid_output = DMMotorCalculateExternalOutput(motor);
+        const float torque_des = pid_output + motor->mit_config.torque_des;
 
-        motor->motor_can_instace->tx_buff[0] = (uint8_t)(motor_send_mailbox.position_des >> 8);
-        motor->motor_can_instace->tx_buff[1] = (uint8_t)(motor_send_mailbox.position_des);
-        motor->motor_can_instace->tx_buff[2] = (uint8_t)(motor_send_mailbox.velocity_des >> 4);
-        motor->motor_can_instace->tx_buff[3] = (uint8_t)(((motor_send_mailbox.velocity_des & 0xF) << 4) | (motor_send_mailbox.Kp >> 8));
-        motor->motor_can_instace->tx_buff[4] = (uint8_t)(motor_send_mailbox.Kp);
-        motor->motor_can_instace->tx_buff[5] = (uint8_t)(motor_send_mailbox.Kd >> 4);
-        motor->motor_can_instace->tx_buff[6] = (uint8_t)(((motor_send_mailbox.Kd & 0xF) << 4) | (motor_send_mailbox.torque_des >> 8));
-        motor->motor_can_instace->tx_buff[7] = (uint8_t)(motor_send_mailbox.torque_des);
+        /*
+         * External impedance mode:
+         * the IMU/PID chain owns the control torque, while the DM internal
+         * position and velocity gains remain disabled.
+         */
+        DMMotorSendMIT(motor, 0.0f, 0.0f, 0.0f, 0.0f, torque_des);
+    }
+}
 
-        CANTransmit(motor->motor_can_instace, 1);
+void DMMotorMITControl(void)
+{
+    for (size_t i = 0; i < idx; ++i)
+    {
+        DMMotorInstance *motor = dm_motor_instance[i];
+
+        /*
+         * MIT mode:
+         * the application supplies p/v/Kp/Kd/tau_ff through
+         * DMMotorSetMITConfig(), and this function sends them without
+         * calculating the external PID chain.
+         */
+        DMMotorSendMIT(
+            motor,
+            motor->mit_config.position_des,
+            motor->mit_config.velocity_des,
+            motor->mit_config.kp,
+            motor->mit_config.kd,
+            motor->mit_config.torque_des);
     }
 }
--- modules/motor/DMmotor/dmmotor.h
+++ modules/motor/DMmotor/dmmotor.h
@@ -1,20 +1,28 @@
 #ifndef DMMOTOR_H
 #define DMMOTOR_H
+
 #include <stdint.h>
+
 #include "bsp_can.h"
 #include "controller.h"
 #include "motor_def.h"
+#include "daemon.h"
 
 #define DM_MOTOR_CNT 4
 
-#define DM_P_MIN  (-12.5f)
-#define DM_P_MAX  12.5f
-#define DM_V_MIN  (-45.0f)
-#define DM_V_MAX  45.0f
-#define DM_T_MIN  (-18.0f)
-#define DM_T_MAX   18.0f
+/* DM4310 protocol limits */
+#define DM_P_MIN (-12.5f)
+#define DM_P_MAX (12.5f)
+#define DM_V_MIN (-20.94f)
+#define DM_V_MAX (20.94f)
+#define DM_T_MIN (-12.5f)
+#define DM_T_MAX (12.5f)
+#define DM_KP_MIN (0.0f)
+#define DM_KP_MAX (500.0f)
+#define DM_KD_MIN (0.0f)
+#define DM_KD_MAX (5.0f)
 
-typedef struct 
+typedef struct
 {
     uint8_t id;
     uint8_t state;
@@ -25,8 +33,9 @@ typedef struct
     float T_Mos;
     float T_Rotor;
     int32_t total_round;
-}DM_Motor_Measure_s;
+} DM_Motor_Measure_s;
 
+/* Encoded MIT command fields used by the CAN packet. */
 typedef struct
 {
     uint16_t position_des;
@@ -34,45 +43,103 @@ typedef struct
     uint16_t torque_des;
     uint16_t Kp;
     uint16_t Kd;
-}DMMotor_Send_s;
+} DMMotor_Send_s;
 
-typedef struct 
+/* Physical-unit MIT command configuration. */
+typedef struct
+{
+    float position_des; /* rad */
+    float velocity_des; /* rad/s */
+    float torque_des;   /* N*m */
+    float kp;
+    float kd;
+} DMMotor_MIT_Config_s;
+
+typedef struct
 {
     DM_Motor_Measure_s measure;
     Motor_Control_Setting_s motor_settings;
+
     PIDInstance current_PID;
     PIDInstance speed_PID;
     PIDInstance angle_PID;
+
     float *other_angle_feedback_ptr;
     float *other_speed_feedback_ptr;
     float *speed_feedforward_ptr;
     float *current_feedforward_ptr;
     float pid_ref;
+
+    /* Used by DMMotorMITControl(). */
+    DMMotor_MIT_Config_s mit_config;
+    DMMotor_Send_s motor_send_mailbox;
+
     Motor_Working_Type_e stop_flag;
     CANInstance *motor_can_instace;
+    DaemonInstance *motor_daemon;
     uint32_t lost_cnt;
-}DMMotorInstance;
+} DMMotorInstance;
 
 typedef enum
 {
-    DM_CMD_MOTOR_MODE = 0xfc,
-    DM_CMD_RESET_MODE = 0xfd,
-    DM_CMD_ZERO_POSITION = 0xfe,
-    DM_CMD_CLEAR_ERROR = 0xfb
-}DMMotor_Mode_e;
+    DM_CMD_MOTOR_MODE = 0xfc,   /* Enable and respond to commands. */
+    DM_CMD_RESET_MODE = 0xfd,   /* Stop. */
+    DM_CMD_ZERO_POSITION = 0xfe, /* Set current position as encoder zero. */
+    DM_CMD_CLEAR_ERROR = 0xfb   /* Clear motor over-temperature error. */
+} DMMotor_Mode_e;
 
 DMMotorInstance *DMMotorInit(Motor_Init_Config_s *config);
 
 void DMMotorSetRef(DMMotorInstance *motor, float ref);
-
-void DMMotorOuterLoop(DMMotorInstance *motor, Closeloop_Type_e closeloop_type);
-
+void DMMotorOuterLoop(DMMotorInstance *motor, Closeloop_Type_e close_loop_type);
 void DMMotorEnable(DMMotorInstance *motor);
-
 void DMMotorStop(DMMotorInstance *motor);
-
 void DMMotorCaliEncoder(DMMotorInstance *motor);
 
+/**
+ * @brief Change the feedback source used by the external PID loops.
+ */
+void DMMotorChangeFeed(
+    DMMotorInstance *motor,
+    Closeloop_Type_e loop,
+    Feedback_Source_e type);
+
+/**
+ * @brief Configure the physical-unit MIT command.
+ *
+ * This command is used by DMMotorMITControl(). In DMMotorControl(), only
+ * torque_des is used as external torque feedforward; p/v/Kp/Kd are zeroed.
+ */
+void DMMotorSetMITConfig(
+    DMMotorInstance *motor,
+    const DMMotor_MIT_Config_s *config);
+
+/**
+ * @brief Set the physical-unit MIT command.
+ */
+void DMMotorSetMITRef(
+    DMMotorInstance *motor,
+    float position_des,
+    float velocity_des,
+    float kp,
+    float kd,
+    float torque_ff);
+
+/**
+ * @brief IMU external impedance control.
+ *
+ * The external angle/speed/current PID chain is calculated in this function.
+ * The transmitted MIT fields are p=0, v=0, Kp=0, Kd=0 and torque=PID output
+ * plus torque feedforward.
+ */
 void DMMotorControl(void);
 
-#endif // !DMMOTOR
+/**
+ * @brief IMU outer loop plus DM internal MIT position/speed loop control.
+ *
+ * The MIT command configured by DMMotorSetMITConfig() is sent directly. The
+ * external PID chain is not calculated in this function.
+ */
+void DMMotorMITControl(void);
+
+#endif /* DMMOTOR_H */
